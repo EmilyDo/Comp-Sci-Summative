303e741ba05bee0d10d8320f8f541df1abdeeba8
Elizabeth Li <eli13@ocdsb.com>
2019-12-05 23:53:56 -0500

    Adding bonus problem page for teacher (can upload images for problem and solution)

---
+++ js/js-for-showing-solution.js
@@ -0,0 +1,8 @@
+function showSolution() {
+    var x = document.getElementbyId("solutionImage");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
